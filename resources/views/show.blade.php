8a20736dddbc8879af2c2877abbe0c043ddc150d
ghanikhan1 <usmanatbit@gmail.com>
2023-10-02 22:25:37 +0100

    second commit

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -12,4 +12,12 @@
 
     <p>{{ $task -> created_at }}</p>
     <p>{{ $task -> updated_at }}</p>
+
+    <div>
+        <form action="{{route('task.destroy', ['task' => $task->id])}}" method="post">
+        @csrf 
+        @method('DELETE')
+        <button type="submit">Click to Delete</button>
+        </form>
+    </div>
 @endsection
